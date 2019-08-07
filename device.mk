81354983963d01448c25c4e8f749f23e6643f00a
taran107 <hora.cooltaran@gmail.com>
2019-08-07 13:05:03 +0530

    raphael: Update system.prop & added vendor_prop.mk

--- device.mk
+++ device.mk
@@ -9,6 +9,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/raphael/raphael-vendor.mk)
 
+# Vendor properties
+-include $(LOCAL_PATH)/vendor_prop.mk
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
